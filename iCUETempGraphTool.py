b6a38ca6e10aeab71cf300d5753c016d7683f554
ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2023-09-10 16:43:47 -0700

    ISSUE: With big files the values on the left are inaccurate.

--- iCUETempGraphTool.py
+++ iCUETempGraphTool.py
@@ -1,31 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import filedialog
-import tkinter as tk
 import os
-from dateutil import parser
 
 # user chooses CSV file to be read
 file_path = filedialog.askopenfilename(title="Select CSV file")
 if not file_path:
     exit()
 
-# specify date and time formats to handle both 12-hour and 24-hour time formats
-date_formats = ["%m/%d/%Y %I:%M:%S %p", "%m/%d/%Y %H:%M:%S"]
-
-# attempt to parse Timestamp using different date formats
-df = None
-for date_format in date_formats:
-    try:
-        df = pd.read_csv(file_path, parse_dates=["Timestamp"], date_format=date_format)
-        break  # successfully parsed
-    except pd.errors.OutOfBoundsDatetime:
-        pass  # continue to the next format if parsing fails
-
-# check if parsing was successful
-if df is None:
-    print("Unable to parse the 'Timestamp' column. Please check the date format in your CSV file.")
-    exit()
+# do not parse timestamp
+df = pd.read_csv(file_path)
 
 # make user select output directory
 output_directory = filedialog.askdirectory(title="Select Output Directory")
@@ -36,26 +20,44 @@ if not output_directory:
 columns_to_plot = df.columns[1:]
 
 # create new figure for every column and save them all seperately
-for column in columns_to_plot:
-    fig, ax = plt.subplots(figsize=(12, 6))
-    ax.plot(df['Timestamp'], df[column])
-    ax.set_title(f'{column} over Time')
-    ax.set_xlabel('Timestamp')
-    ax.set_ylabel(column)
-    ax.grid(True)
+batch_size = 10 
+for i in range(0, len(columns_to_plot), batch_size):
+    batch_columns = columns_to_plot[i:i + batch_size]
 
-    # rotate bottom time labels for better readability
-    plt.xticks(rotation=45)
+    for column in batch_columns:
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.plot(df['Timestamp'], df[column])
+        ax.set_title(f'{column} over Time')
+        ax.set_xlabel('Timestamp')
+        ax.set_ylabel(column)
 
-    
-    ax.yaxis.set_major_locator(plt.MaxNLocator(nbins=6))  # adjust the number of ticks as needed
+        # rotate bottom time labels for better readability
+        plt.xticks(rotation=45)
 
-    # generate the filename based on the column name
-    filename = f"{column.replace(' ', '_')}.png"
-    save_path = os.path.join(output_directory, filename)
+        # get the unique values in the column and sort them
+        unique_values = sorted(df[column].unique())
 
-    plt.tight_layout()
-    plt.savefig(save_path)
-    plt.close()
+        # set custom y-axis labels with adjusted spacing
+        y_ticks = [unique_values[i] for i in range(0, len(unique_values), len(unique_values) // 10)]
+        ax.set_yticks(y_ticks)
+
+        # how to explain 2 electric boogaloo
+        n = max(len(df) // 10, 1)
+        ax.set_xticks(df['Timestamp'].iloc[::n])
+
+        # disable the background grid
+        ax.grid(False)
+
+        # calculate highest and lowest value for this column
+        highest_value = df[column].max()
+        lowest_value = df[column].min()
+
+        # generate file name based on the column name
+        filename = f"{column.replace(' ', '_')}.png"
+        save_path = os.path.join(output_directory, filename)
+
+        plt.tight_layout()
+        plt.savefig(save_path)
+        plt.close()
 
 print("Graphs saved successfully.")
