0dcb4e91ce2c776b54b1b0d697b805ab44682f94
ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2023-09-10 12:58:01 -0700

    Not everybody is american!
    24 hour time and a different date format would crash everything lmao

--- iCUETempGraphTool.py
+++ iCUETempGraphTool.py
@@ -3,37 +3,52 @@ import matplotlib.pyplot as plt
 from tkinter import filedialog
 import tkinter as tk
 import os
+from dateutil import parser
 
 # user chooses CSV file to be read
 file_path = filedialog.askopenfilename(title="Select CSV file")
 if not file_path:
     exit()
 
-df = pd.read_csv(file_path)
+# specify date and time formats to handle both 12-hour and 24-hour time formats
+date_formats = ["%m/%d/%Y %I:%M:%S %p", "%m/%d/%Y %H:%M:%S"]
 
-# set timestamp as index
-df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-df.set_index('Timestamp', inplace=True)
+# attempt to parse Timestamp using different date formats
+df = None
+for date_format in date_formats:
+    try:
+        df = pd.read_csv(file_path, parse_dates=["Timestamp"], date_format=date_format)
+        break  # successfully parsed
+    except pd.errors.OutOfBoundsDatetime:
+        pass  # continue to the next format if parsing fails
+
+# check if parsing was successful
+if df is None:
+    print("Unable to parse the 'Timestamp' column. Please check the date format in your CSV file.")
+    exit()
 
 # make user select output directory
 output_directory = filedialog.askdirectory(title="Select Output Directory")
 if not output_directory:
     exit()
 
-# select columns to plot
+# select columns to plot but excluding the timestamp
 columns_to_plot = df.columns[1:]
 
-# plot graphs
+# create new figure for every column and save them all seperately
 for column in columns_to_plot:
     fig, ax = plt.subplots(figsize=(12, 6))
-    ax.plot(df.index, df[column])
+    ax.plot(df['Timestamp'], df[column])
     ax.set_title(f'{column} over Time')
     ax.set_xlabel('Timestamp')
     ax.set_ylabel(column)
     ax.grid(True)
 
+    # rotate bottom time labels for better readability
+    plt.xticks(rotation=45)
+
     # idek how to explain this lmao
-    ax.yaxis.set_major_locator(plt.MaxNLocator(nbins=6))  # adjust number of ticks as needed
+    ax.yaxis.set_major_locator(plt.MaxNLocator(nbins=6))  # adjust the number of ticks as needed
 
     # generate the filename based on the column name
     filename = f"{column.replace(' ', '_')}.png"
