f3f578df5aad5c014e726388494759579576c7cb
ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2023-09-12 09:25:18 -0700

    Fixed inaccurate y-axis graph.
    Timestamp is now wrong tho.

--- iCUETempGraphTool.py
+++ iCUETempGraphTool.py
@@ -2,62 +2,77 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import filedialog
 import os
+import matplotlib.ticker as ticker
 
-# user chooses CSV file to be read
+# Ask the user to select a CSV file using a file dialog
 file_path = filedialog.askopenfilename(title="Select CSV file")
+
+# If the user cancels the file dialog, exit the program
 if not file_path:
     exit()
 
-# do not parse timestamp
-df = pd.read_csv(file_path)
+# Define the date format used in the CSV file
+date_format = "%m/%d/%Y %H:%M:%S %p"
 
-# make user select output directory
+# Read the CSV file into a DataFrame and parse the "Timestamp" column as dates
+df = pd.read_csv(file_path, parse_dates=["Timestamp"])
+
+# Ask the user to select an output directory using a directory dialog
 output_directory = filedialog.askdirectory(title="Select Output Directory")
+
+# If the user cancels the directory dialog, exit the program
 if not output_directory:
     exit()
 
-# select columns to plot but excluding the timestamp
+# Get a list of columns in the DataFrame to create plots for
 columns_to_plot = df.columns[1:]
 
-# create new figure for every column and save them all seperately
-batch_size = 10 
-for i in range(0, len(columns_to_plot), batch_size):
-    batch_columns = columns_to_plot[i:i + batch_size]
+# Loop through each column and create a plot
+for column in columns_to_plot:
+    # Create a new figure for each plot with specified dimensions
+    plt.figure(figsize=(12, 6))
 
-    for column in batch_columns:
-        fig, ax = plt.subplots(figsize=(12, 6))
-        ax.plot(df['Timestamp'], df[column])
-        ax.set_title(f'{column} over Time')
-        ax.set_xlabel('Timestamp')
-        ax.set_ylabel(column)
+    # Plot the data using the "Timestamp" column as x-axis and the current column as y-axis
+    plt.plot(df['Timestamp'], df[column])
 
-        # rotate bottom time labels for better readability
-        plt.xticks(rotation=45)
+    # Set the title, x-axis label, and y-axis label for the plot
+    plt.title(f'{column} over Time')
+    plt.xlabel('Timestamp')
+    plt.ylabel(column)
 
-        # get the unique values in the column and sort them
-        unique_values = sorted(df[column].unique())
+    # Add a grid to the plot
+    plt.grid(True)
 
-        # set custom y-axis labels with adjusted spacing
-        y_ticks = [unique_values[i] for i in range(0, len(unique_values), len(unique_values) // 10)]
-        ax.set_yticks(y_ticks)
+    # Adjust the layout of the plot
+    plt.tight_layout()
 
-        # how to explain 2 electric boogaloo
-        n = max(len(df) // 10, 1)
-        ax.set_xticks(df['Timestamp'].iloc[::n])
+    # Format the x-axis labels as dates
+    plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%m/%d/%Y %H:%M:%S %p'))
 
-        # disable the background grid
-        ax.grid(False)
+    # Rotate the x-axis labels for better readability
+    plt.xticks(rotation=10)
 
-        # calculate highest and lowest value for this column
-        highest_value = df[column].max()
-        lowest_value = df[column].min()
+    # Get unique labels from the current column and sort them numerically
+    y_labels = df[column].unique()
+    sorted_y_labels = sorted(y_labels, key=lambda label: float(''.join(filter(str.isdigit, label))))
 
-        # generate file name based on the column name
-        filename = f"{column.replace(' ', '_')}.png"
-        save_path = os.path.join(output_directory, filename)
+    # Configure the y-axis tick locations and labels
+    loc = ticker.MultipleLocator(base=max(1, len(sorted_y_labels) // 10))
+    plt.gca().yaxis.set_major_locator(loc)
+    plt.gca().set_yticklabels(sorted_y_labels)
 
-        plt.tight_layout()
-        plt.savefig(save_path)
-        plt.close()
+    # Generate the filename for the saved plot
+    filename = f"{column.replace(' ', '_')}.png"
+    save_path = os.path.join(output_directory, filename)
 
+    # Save the plot as an image file
+    plt.savefig(save_path)
+
+    # Close the current plot to free up resources
+    plt.close()
+
+# Inform the user that the graphs have been saved successfully
 print("Graphs saved successfully.")
+
+
+# The comments were added by an AI because I am horrible at leaving code comments.
