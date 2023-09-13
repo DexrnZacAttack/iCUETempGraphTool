5d06cff0ae174e838e15c101bf3fe677ad713e70
ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2023-09-12 21:17:31 -0700

    Fix timestamp issue + Verbosity + Wait 10 seconds before exiting.

--- iCUETempGraphTool.py
+++ iCUETempGraphTool.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 from tkinter import filedialog
 import os
 import matplotlib.ticker as ticker
+import time
+
+print("[INFORMATION] If you find any issues or would like to look at the code/repo,\n go to https://github.com/dexrnzacattack/iCueTempGraphTool")
 
 # Ask the user to select a CSV file using a file dialog
-print("[MESSAGE] Please select a CSV file.")
+print("\n[MESSAGE] Please select a CSV file.")
 file_path = filedialog.askopenfilename(title="Select CSV file")
 
 # If the user cancels the file dialog, exit the program
 if not file_path:
-    print("[ERROR] No file selected, Exiting.")
+    print("\n[ERROR] No file selected, Exiting.")
     exit()
  
     
@@ -27,21 +30,21 @@ df = pd.read_csv(file_path, parse_dates=["Timestamp"])
 # Get a list of columns in the DataFrame to create plots for
 columns_to_plot = df.columns[1:]
 
-# Define and print column and row amount
+# Define and print column, row amount and file size
 ColumnAmount = len(columns_to_plot)
 RowAmount = df.shape[0]
 FileSize = os.path.getsize(file_path)
-print(f'[DEBUG] There are {ColumnAmount} columns in the CSV file.')
+print(f'\n[DEBUG] There are {ColumnAmount} columns in the CSV file.')
 print(f'[DEBUG] There are {RowAmount} rows in the CSV file.')
-print(f'[DEBUG] The file is {FileSize}b in size.')
+print(f'[DEBUG] The file is {FileSize}B in size.')
 
 # Ask the user to select an output directory using a directory dialog
-print("[MESSAGE] Please select a output folder.")
+print("\n[MESSAGE] Please select a output folder.")
 output_directory = filedialog.askdirectory(title="Select Output Directory")
 
 # If the user cancels the directory dialog, exit the program
 if not output_directory:
-    print("[ERROR] No folder selected, Exiting.")
+    print("\n[ERROR] No folder selected, Exiting.")
     exit()
 
 # Loop through each column and create a plot
@@ -70,15 +73,10 @@ for column in columns_to_plot:
     # Rotate the x-axis labels for better readability
     plt.xticks(rotation=10)
 
-    # Get unique labels from the current column and sort them numerically
-    y_labels = df[column].unique()
-    sorted_y_labels = sorted(y_labels, key=lambda label: float(''.join(filter(str.isdigit, label))))
+    # Use MaxNLocator to limit the number of X-axis labels
+    locator = ticker.MaxNLocator(nbins=10)
+    plt.gca().xaxis.set_major_locator(locator)
 
-    # Configure the y-axis tick locations and labels
-    loc = ticker.MultipleLocator(base=max(1, len(sorted_y_labels) // 10))
-    plt.gca().yaxis.set_major_locator(loc)
-    plt.gca().set_yticklabels(sorted_y_labels)
-    
     # Increment ColumnsComplete so that the print command will show when each column is complete.
     ColumnsComplete += 1
     print(f'[DEBUG] Column {ColumnsComplete} Complete')
@@ -94,7 +92,8 @@ for column in columns_to_plot:
     plt.close()
 
 # Inform the user that the graphs have been saved successfully
-print("[SUCCESS] Graphs saved successfully.")
-
+print("\n[SUCCESS] Graphs saved successfully.")
+print(f"\n[INFORMATION] Tool will exit in 10 seconds.")
+time.sleep(10)
 
 # The comments were added by an AI because I am horrible at leaving code comments.
